41dd694038516c1effbf41aa94d6aab40592a801
tpollock4545 <93632954+tpollock4545@users.noreply.github.com>
2023-06-03 12:09:55 -0700

    upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express"); 
+const MongoClient = require("mongodb").MongoClient; 
+const cors = require("cors"); 
+const bodyParser = require("body-parser"); 
+const path = require("path"); 
+const cookieParser = require("cookie-parser"); 
+const mime = require("mime-types"); 
+require("dotenv").config(); 
+
+const MONGODB_URI = process.env.MONGODB_URI; 
+const DB_NAME = "TradingBotDB"; 
+const COLLECTION_NAME = "ScoidData"; 
+const PORT = process.env.PORT || 3000; 
+
+const app = express(); 
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(cookieParser()); 
+
+let client; 
+
+async function connectToMongo() { 
+try { 
+    client = await MongoClient.connect(MONGODB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    }); 
+} catch (error) { 
+    console.error("Error connecting to MongoDB:", error); 
+    setTimeout(connectToMongo, 5000); // Attempt to reconnect after 5 seconds 
+} 
+} 
+
+connectToMongo(); 
+
+async function getScoidCollection() { 
+if (!client || !client.topology.isConnected()) { 
+    await connectToMongo(); 
+} 
+
+return client.db(DB_NAME).collection(COLLECTION_NAME); 
+}
+
+
+
+app.get("/allData", async (req, res) => {
+    const collection = await getScoidCollection();
+    const scoid = await collection.find({}).toArray();
+    res.send(scoid);
+});
+
+app.post("/setSymbols", async (req, res) => {
+    const collection = await getScoidCollection();
+    let symbolList = req.body;
+
+    // Get all existing symbols
+    const existingSymbols = await collection.find({}, { projection: { symbol: 1, _id: 0 } }).toArray();
+
+    // Create a Set of existing symbols for efficient lookup
+    const existingSymbolsSet = new Set(existingSymbols.map(s => s.symbol));
+
+    // For each input symbol, if it doesn't exist in the Set, insert it into the database
+    for (const symbol of symbolList) {
+        if (!existingSymbolsSet.has(symbol)) {
+            await collection.insertOne({ symbol: symbol, data: {} });
+        }
+    }
+
+    res.send({ message: 'Symbols processed successfully.' });
+});
+
+
+
+
+
+
+
+app.listen(PORT, () => { 
+    console.log(`Server listening on port ${PORT}`); 
+    }); 
+
+
